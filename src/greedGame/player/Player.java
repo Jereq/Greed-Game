15821157762701cad364e67a2cfa545799f8bbc6
Roffe <Roffe@Roffe-Dator>
2011-12-14 19:54:09 +0100

    tried to do some methods in player interface

--- src/greedGame/player/Player.java
+++ src/greedGame/player/Player.java
@@ -2,4 +2,18 @@ package greedGame.player;
 
 public interface Player {
 
+	private int getScore()
+	{
+		
+	};
+	
+	private void addScore(points, int)
+	{
+		
+	};
+	
+	private String getName()
+	{
+		
+	};
 }
