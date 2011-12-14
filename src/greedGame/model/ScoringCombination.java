e5717362513f70ceb957a597a5f73f758f64e490
Sebastian Larsson <sjereq@gmail.com>
2011-12-14 20:32:10 +0100

    Implemented ScoringCombination

--- src/greedGame/model/ScoringCombination.java
+++ src/greedGame/model/ScoringCombination.java
@@ -1,5 +1,32 @@
 package greedGame.model;
 
+import java.util.List;
+
 public class ScoringCombination {
 
+	private String name;
+	private int score;
+	private List<Dice> dice;
+	
+	public ScoringCombination(String name, int score, List<Dice> dice) {
+		this.name = name;
+		this.score = score;
+		this.dice = dice;
+	}
+	
+	public String toString() {
+		return name + " : " + score;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public List<Dice> getDice() {
+		return dice;
+	}
+	
+	public String getScoreName() {
+		return name;
+	}
 }
