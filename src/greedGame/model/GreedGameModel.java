9a5d3e2f67059fc2807de086a33e24a12b1ebab0
Sebastian Larsson <sjereq@gmail.com>
2011-12-14 21:21:55 +0100

    Implemented GreedGameModel

--- src/greedGame/model/GreedGameModel.java
+++ src/greedGame/model/GreedGameModel.java
@@ -1,9 +1,39 @@
 package greedGame.model;
 
-public class GreedGameModel {
-	
-	
-	
-	
+import greedGame.player.Player;
 
+import java.util.Iterator;
+import java.util.List;
+
+public class GreedGameModel {
+
+	private List<Player> players;
+	private Iterator<Player> currentPlayerIterator;
+	private Player currentPlayer;
+	private DiceHandler diceHandler;
+
+	public void rollDice() {
+		diceHandler.rollDice();
+	}
+
+	public void bank() {
+		currentPlayer.addScore(0); // TODO: Calculate score
+		nextPlayer();
+	}
+	
+	private void nextPlayer() {
+		if (!currentPlayerIterator.hasNext())
+			currentPlayerIterator = players.iterator();
+
+		currentPlayer = currentPlayerIterator.next();
+	}
+	
+	public void addPlayer(Player player) {
+		players.add(player);
+	}
+	
+	public void removeCurrentPlayer() {
+		currentPlayerIterator.remove();
+		nextPlayer();
+	}
 }
