8758cc8ab4d7267a14ca4c47caf1e89c5d12cb3a
Roffe <Roffe@Roffe-Dator>
2011-12-15 23:40:03 +0100

    just started a little on CowardAIPlayer

--- src/greedGame/model/player/CowardAIPlayer.java
+++ src/greedGame/model/player/CowardAIPlayer.java
@@ -7,4 +7,11 @@ public class CowardAIPlayer extends AIPlayer {
 	public CowardAIPlayer(String name, GreedGameModel model) {
 		super(name, model);
 	}
+	
+	public void Decide()
+	{
+		rollDice();
+		getUnreservedDice();
+		if()
+	}
 }
