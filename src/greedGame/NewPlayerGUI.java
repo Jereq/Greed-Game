f73e52dd42c6460b614b2695bebbd8cc7e8fd5ed
Sebastian Larsson <sjereq@gmail.com>
2011-12-16 23:22:47 +0100

    Altered NewPlayerGUI in preparation of merging with main gui

--- src/greedGame/NewPlayerGUI.java
+++ src/greedGame/NewPlayerGUI.java
@@ -1,36 +1,23 @@
 package greedGame;
 
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.JTextPane;
 import javax.swing.JLabel;
+
+import java.awt.BorderLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class NewPlayerGUI {
 
 	private JFrame frame;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					NewPlayerGUI window = new NewPlayerGUI();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	
+	private JPanel surroundingAddPlayerPanel;
 
 	/**
 	 * Create the application.
@@ -43,14 +30,23 @@ public class NewPlayerGUI {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		initializeAddPlayerPanel();
+	}
+	
+	private void initializeAddPlayerPanel() {
+		
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		surroundingAddPlayerPanel = new JPanel();
+		frame.getContentPane().add(surroundingAddPlayerPanel);
+		surroundingAddPlayerPanel.setLayout(null);
 		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setBounds(32, 44, 113, 20);
-		frame.getContentPane().add(comboBox);
+		surroundingAddPlayerPanel.add(comboBox);
 		
 		JButton btnCreate = new JButton("Create Player");
 		btnCreate.addActionListener(new ActionListener() {
@@ -58,16 +54,16 @@ public class NewPlayerGUI {
 			}
 		});
 		btnCreate.setBounds(155, 43, 100, 23);
-		frame.getContentPane().add(btnCreate);
+		surroundingAddPlayerPanel.add(btnCreate);
 		
 		JButton btnCancel = new JButton("Cancel/Continue");
 		btnCancel.setBounds(265, 228, 159, 23);
-		frame.getContentPane().add(btnCancel);
+		surroundingAddPlayerPanel.add(btnCancel);
 		
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		scrollPane.setBounds(265, 44, 159, 176);
-		frame.getContentPane().add(scrollPane);
+		surroundingAddPlayerPanel.add(scrollPane);
 		
 		JTextPane txtpnMimimimi = new JTextPane();
 		txtpnMimimimi.setText("mimimimi");
@@ -75,6 +71,6 @@ public class NewPlayerGUI {
 		
 		JLabel lblPlayers = new JLabel("Players");
 		lblPlayers.setBounds(265, 19, 46, 14);
-		frame.getContentPane().add(lblPlayers);
+		surroundingAddPlayerPanel.add(lblPlayers);
 	}
 }
