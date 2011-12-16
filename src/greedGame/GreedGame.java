35750b7a0fe5993680392213e566ecfffb708739
Sebastian Larsson <sjereq@gmail.com>
2011-12-16 23:26:47 +0100

    Improved GreedGame's handling of windows

--- src/greedGame/GreedGame.java
+++ src/greedGame/GreedGame.java
@@ -1,26 +1,39 @@
 package greedGame;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import greedGame.model.GreedGameModel;
 
 public class GreedGame {
+		
+	static SetupGUI setup;
+	
+	static GreedGameModel model;
+	static GreedGameGUIExperiment view;
+	static GreedGameController controller;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		
-		GreedGameModel model;
-		GreedGameGUIExperiment view;
-		GreedGameController controller;
-		
 		try {
-			model = new GreedGameModel(300, 10000);
-			view = new GreedGameGUIExperiment(model);
-			model.addObserver(view);
-			
-			controller = new GreedGameController(model, view);
-			
-			view.setVisible(true);
+			setup = new SetupGUI();
+			setup.addStartGameActionListener( new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					setup.setVisible(false);
+					
+					model = new GreedGameModel(setup.getBustLimit(), setup.getPointsToWin());
+					view = new GreedGameGUIExperiment(model);
+					model.addObserver(view);
+					
+					controller = new GreedGameController(model, view);
+					
+					view.setVisible(true);
+				}
+			});
+			setup.setVisible(true);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
