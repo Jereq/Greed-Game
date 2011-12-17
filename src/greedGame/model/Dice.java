5e6562c1d50fe712730f9a21bf9ad8229928fd66
Roffe <Roffe@Roffe-Dator>
2011-12-17 23:44:28 +0100

    added some comments to Dice and DiceHandler

--- src/greedGame/model/Dice.java
+++ src/greedGame/model/Dice.java
@@ -8,28 +8,34 @@ public class Dice implements Comparable<Dice> {
 	private int value = 1;
 	private DiceState state;
 	
+	//
 	public Dice(Random rnd) {
 		this.rnd = rnd;
 		state = DiceState.RESERVED;
 	}
 	
+	//returns the rolled value
 	public int getValue() {
 		return value;
 	}
 	
+	//
 	public void roll() {
 		state = DiceState.FREE;
 		value = rnd.nextInt(6) + 1;
 	}
 	
+	//
 	public DiceState getState() {
 		return state;
 	}
 	
+	//
 	public void setState(DiceState state) {
 		this.state = state;
 	}
-
+	
+	//
 	@Override
 	public int compareTo(Dice o) {
 		return value - o.getValue();
--- src/greedGame/model/DiceHandler.java
+++ src/greedGame/model/DiceHandler.java
@@ -11,6 +11,7 @@ public class DiceHandler {
 	private List<Dice> dice;
 	private ScoringRules rules;
 
+	//
 	public DiceHandler() {
 		dice = new ArrayList<Dice>(numberOfDice);
 		rules = new BasicScoringRules();
@@ -22,6 +23,7 @@ public class DiceHandler {
 		}
 	}
 
+	//
 	public void rollDice() {
 
 		boolean isAllReserved = true;
@@ -39,28 +41,34 @@ public class DiceHandler {
 		}
 	}
 
+	//Returns the maximum amount of points the current dice can give. 
 	public int getMaxPoints() {
 		return rules.getMaxPoints(getUnreservedDice());
 	}
 
+	//Returns the scoring combinations of the current dice.
 	public List<ScoringCombination> getScoringCombinations() {
 		return rules.getScoringCombinations(getSelectedDice());
 	}
 
+	//Sets the state of the selected dice to reserved.
 	public void reserveSelectedDice() {
 		for (Dice d : getSelectedDice()) {
 			d.setState(DiceState.RESERVED);
 		}
 	}
 
+	//
 	public void selectDice(Dice selDice) {
 		selDice.setState(DiceState.SELECTED);
 	}
 
+	//
 	public void unselectDice(Dice selDice) {
 		selDice.setState(DiceState.FREE);
 	}
 
+	//Returns the selected dice
 	public List<Dice> getSelectedDice() {
 
 		ArrayList<Dice> selectedDice = new ArrayList<Dice>(numberOfDice);
@@ -72,6 +80,7 @@ public class DiceHandler {
 		return selectedDice;
 	}
 
+	//Returns the dice that weren't selected before the new roll.
 	public List<Dice> getUnreservedDice() {
 
 		ArrayList<Dice> unreservedDice = new ArrayList<Dice>(numberOfDice);
@@ -83,6 +92,7 @@ public class DiceHandler {
 		return unreservedDice;
 	}
 
+	//Returns the dice that are neither reserved nor selected.
 	public List<Dice> getFreeDice() {
 
 		ArrayList<Dice> freeDice = new ArrayList<Dice>(numberOfDice);
@@ -94,14 +104,17 @@ public class DiceHandler {
 		return freeDice;
 	}
 
+	//
 	public List<Dice> getDice() {
 		return dice;
 	}
 
+	//Returns the scoring rules.
 	public ScoringRules getScoringRules() {
 		return rules;
 	}
 
+	//Reserves all dice.
 	public void reserveAllDice() {
 		for (Dice d : dice)
 			d.setState(DiceState.RESERVED);
