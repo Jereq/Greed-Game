069f604c05ac2bdaae5a3e1261ead90dcfff3f1d
Sebastian Larsson <sjereq@gmail.com>
2011-12-18 17:44:53 +0100

    Commented RandomAIPlayer.java

--- src/greedGame/model/player/RandomAIPlayer.java
+++ src/greedGame/model/player/RandomAIPlayer.java
@@ -4,31 +4,37 @@ import java.util.Random;
 
 import greedGame.model.GreedGameModel;
 
+/**
+ * A randomly deciding implementation of <code>AIPlayer</code>. Banks and keeps
+ * rolling at a 50/50 ration.
+ */
 public class RandomAIPlayer extends AIPlayer {
 
-	Random random;// creates a "Random object".
-	
-	//the class constructor.
+	Random random;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param name
+	 *            The name given to the player
+	 * @param model
+	 *            The model the AI is going to act upon
+	 */
 	public RandomAIPlayer(String name, GreedGameModel model) {
-		super(name, model); 
+		super(name, model);
 		random = new Random();
 	}
-	
+
 	@Override
 	protected void decide() {
-		selectAllCombinations(); //selects the dice to keep or bank.
-		
-		// random boolean if statement to make the random AI either keep rolling or bank his score.
-		if (random.nextBoolean()) 
-		{
+		selectAllCombinations(); // selects all dice to keep or bank.
+
+		// random boolean if statement to make the random AI either keep rolling
+		// or bank his score.
+		if (random.nextBoolean()) {
 			setDecision(AIDecision.KEEP_ROLLING);
-		}
-		else
-		{
+		} else {
 			setDecision(AIDecision.BANK);
 		}
-		
-
 	}
-
 }
