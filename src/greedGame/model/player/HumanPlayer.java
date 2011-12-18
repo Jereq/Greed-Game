beaaa43bd8791afa5992f47f1f55d2b320448589
Sebastian Larsson <sjereq@gmail.com>
2011-12-18 17:26:49 +0100

    Commented HumanPlayer.java

--- src/greedGame/model/player/HumanPlayer.java
+++ src/greedGame/model/player/HumanPlayer.java
@@ -1,50 +1,56 @@
 package greedGame.model.player;
 
+/**
+ * A basic human player implementation of <code>Player</code>. It is a player
+ * using the GUI, so this class does very little.
+ */
 public class HumanPlayer implements Player {
-	
+
 	private String name;
 	private int score;
-	
-	//
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param name
+	 *            The name given to the player
+	 */
 	public HumanPlayer(String name) {
 		this.name = name;
 		score = 0;
 	}
 
-	//Returns the players score.
 	@Override
 	public int getScore() {
 		return score;
 	}
 
-	//Adds the current subscore to the players total.
 	@Override
 	public void addScore(int points) {
 		score += points;
 	}
 
-	//Returns the players name.
 	@Override
 	public String getName() {
 		return name;
 	}
 
-	
 	@Override
 	public void beginTurn() {
 		// Calling human... Turn begun!
+		// A human player can see his/her turn has begun in the GUI.
 	}
 
-	//
 	@Override
 	public boolean isLocalGUIPlayer() {
+		// A human player needs to use the GUI.
 		return true;
 	}
 
-	
 	@Override
 	public void beginDecide() {
-		// Waiting for response from gui...
+		// Waiting for response from GUI...
+		// A human player can see his/her possibilities in the GUI.
 	}
 
 }
