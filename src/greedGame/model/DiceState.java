403ff6c08bca075dfdf288871dcd23217f44f68c
Sebastian Larsson <sjereq@gmail.com>
2011-12-18 21:18:23 +0100

    Commented DiceState.java

--- src/greedGame/model/DiceState.java
+++ src/greedGame/model/DiceState.java
@@ -1,8 +1,21 @@
 package greedGame.model;
 
-//An enumeration containing the possible sates a dice can be in.
+/**
+ * An enumeration defining the possible states a dice can be in.
+ */
 public enum DiceState {
+	/**
+	 * The dice is free to roll or select for scoring.
+	 */
 	FREE,
+
+	/**
+	 * The dice is selected for scoring
+	 */
 	SELECTED,
+	
+	/**
+	 * The dice is reserved, and can not normally be rolled or used for scoring.
+	 */
 	RESERVED
 }
